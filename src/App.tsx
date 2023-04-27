081c821845452635fd68d7afe9e18622077eb171
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-04-28 04:12:46 +0900

    test dragdrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import './App.css';
+import { DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    {id:0, text:'item0'}, 
+    {id:1, text:'item1'}, 
+    {id:2, text:'item2'}
+  ]);
+
+  const onDragEnd = (result:any) => {
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
+
   return (
-    <div className="App">
-      <h1>タスク管理</h1>
+    <div className='dragDropArea'>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {items.map((item, index) => (
+                <Draggable draggableId={item.text} index={index} key={item.id}>
+                  {(provided) => (
+                    <div className='item'
+                    ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      {item.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
 
-export default App;
+export default App
